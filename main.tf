cfb5afec6a70c4c8bbfbccef557e20400adbaf76
Pinaki Mukherjee <pinaki@mukherjee.com>
2022-10-04 13:45:24 +1100

    Update

---
+++ main.tf
@@ -0,0 +1,26 @@
+# The configuration for the `remote` backend.
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  backend "remote" {
+    organization = "pinaki_test"
+
+    workspaces {
+      name = "blog-infra"
+    }
+  }
+}
+
+# An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
